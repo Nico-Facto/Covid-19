bc76016df0342d6594c3253082a12d87aed4ab43
Nicolas Autexier <nicolas.atx@gmx.fr>
2020-10-09 14:38:22 +0200

    Cron Work

--- daily_clock.py
+++ daily_clock.py
@@ -2,15 +2,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour='12', minute='36')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='12', minute='00')
 def timed_job():
     print("---------- JOB START ----------")
     import daily_job
     print("---------- JOB OVER ----------")
 
-
-@sched.scheduled_job('interval', minutes=5)
-def timed_job_2():
-    print(' *** - *** This job is run every one minutes. *** - ***')
 
 sched.start()
