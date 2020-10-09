736ad0a0a37e899730d3ed0ebef7d054c064d0ad
Nicolas Autexier <nicolas.atx@gmx.fr>
2020-10-09 13:57:03 +0200

    SetCron3

--- daily_clock.py
+++ daily_clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron',day_of_week='mon-sun' hour='16', minute='05')
+@sched.scheduled_job('cron',day_of_week='mon-sun', hour='16', minute='05')
 def timed_job():
     import daily_job
 
